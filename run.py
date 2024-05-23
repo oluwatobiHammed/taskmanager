c715ae1c236d5e9e4342316ac49cb9b521877823
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-22 23:11:15 -0400

    implemented Viewing Categories
    - Create project database table
    - create get category route

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
-from taskmanager import app
+from taskmanager import app, db
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    print("Tables created.")
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,7 +1,33 @@
-from flask import render_template
+from flask import flash, render_template, request, redirect, url_for
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
+
+@app.route("/categories")
+def categories():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+ if request.method == 'POST':
+        category_name = request.form.get('category_name')
+        existing_category = Category.query.filter_by(category_name=category_name).first()
+        
+        if existing_category:
+            flash(f'Category {category_name} already exists.', 'warning')
+        else:
+            new_category = Category(category_name=category_name)
+            db.session.add(new_category)
+            db.session.commit()
+            flash(f'Category {category_name} added successfully!', 'success')
+        
+        return redirect(url_for('categories'))
+    
+ return render_template('add_category.html')
+
+ 
