25505aab49f5f4a7cfe9f819f1a1d9d72d248792
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-24 17:04:20 -0400

    updated database configuration

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if os.environ.get("DEVELOPMENT") == "True":
         with app.app_context():
             db.create_all()
-    print("Tables created.")
+        print("Tables created.")
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
