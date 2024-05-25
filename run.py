06caaafabca9da7dcf10d3c3db02869a9c07d9ae
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-25 11:55:29 -0400

    adjusted the data url setup

--- run.py
+++ run.py
@@ -3,9 +3,8 @@ from taskmanager import app, db
 
 
 if __name__ == "__main__":
-    if os.environ.get("DEVELOPMENT") == "True":
-        with app.app_context():
-            db.create_all()
+    with app.app_context():
+        db.create_all()
         print("Tables created.")
     app.run(
         host=os.environ.get("IP"),
--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,10 +10,13 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-   uri = os.environ.get("DATABASE_URL")
-   if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
-         app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    uri = os.environ.get("DATABASE_URL")
+
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        print(f"url: {uri}")
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+        
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
