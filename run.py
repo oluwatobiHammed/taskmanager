65dd4a2bcec63edf3dab91712c36deddcfc50d4e
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-25 21:53:22 -0400

    updated Procfile,  requirements.txt,  added new app.py file
    i am try to run my app on mongodb

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from taskmanager import app, db
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        print("Tables created.")
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
